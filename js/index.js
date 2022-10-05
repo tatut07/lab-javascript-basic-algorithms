2d3509aa6cd0890fa5d7d7cc5b209179c9623f6e
Tatiana Toledo <tatiana@Tatianas-MacBook-Pro.local>
2022-10-05 12:36:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+console.log("I'm ready!");
 // Iteration 1: Names and Input
 //
 // Iteration 2: Conditionals
 
-
 // Iteration 3: Loops
+const hacker1 = "Jane Doe";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Joe Doe";
+console.log(`The navigator's name is ${hacker2}`);
